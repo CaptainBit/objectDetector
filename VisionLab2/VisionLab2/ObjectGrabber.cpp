c71656a0669ed24ca03e53a0dbf639e07e2b0200
Isaac Fiset <34320860+CaptainBit@users.noreply.github.com>
2018-09-18 13:14:30 -0400

    Change logic to work with labeling loop

--- VisionLab2/VisionLab2/ObjectGrabber.cpp
+++ VisionLab2/VisionLab2/ObjectGrabber.cpp
@@ -10,40 +10,72 @@ void ObjectGrabber::setMat(Mat imgToSet)
 int ObjectGrabber::getNumberObject() 
 {
 	int nbObject = 0;
-	int** tabColor = new int*[_img.rows, _img.cols];
-
-	for (int col = 1; col < _img.cols; col++) 
+	Mat tabColor = _img.clone();
+	tabColor.setTo(0);
+	int minimum;
+	for (int row = 1; row < _img.rows; row++) 
 	{
 
-		for (int row = 1; row < _img.rows; row++ )
+		for (int col = 1; col < _img.cols; col++ )
 		{
 			uchar middle = _img.at<uchar>(row, col);
-			uchar left = _img.at<uchar>(row, col -1);
-			uchar top = _img.at<uchar>(row -1, col);
+			uchar left = tabColor.at<uchar>(row, col - 1);
+			uchar top = tabColor.at<uchar>(row - 1, col);
 
 			//si pixel est blanche
 			if (middle == 255) 
 			{
-				//si pixel correspond
-				if (tabColor[row - 1, col] != 0)
+				if (left != 0 || top != 0)
 				{
-					tabColor[row, col] = tabColor[row - 1, col];
-				}
-				else
-				{
-					if (tabColor[row, col - 1] != 0)
-					{
-						tabColor[row, col] = tabColor[row, col - 1];
+					minimum = min(left, top);
+					if (minimum == 0) {
+						tabColor.at<uchar>(row, col) = max(left, top);
 					}
 					else
 					{
-						nbObject++;
-						tabColor[row, col] = new int[nbObject];
+						tabColor.at<uchar>(row, col) = minimum;
 					}
 				}
+				else
+				{
+					nbObject++;
+					tabColor.at<uchar>(row, col) = nbObject;
+				}
+				
 			}
 			
 		}
 	}
-	return 0;
+	int imax = 0;
+	for (int row = _img.rows -2 ; row >= 0; row--)
+	{
+
+		for (int col = _img.cols -2 ; col >= 0; col--)
+		{
+			uchar middle = _img.at<uchar>(row, col);
+			uchar right = tabColor.at<uchar>(row, col + 1);
+			uchar bottom = tabColor.at<uchar>(row +1, col);
+
+			//si pixel est blanche
+			if (middle == 255)
+			{
+				if (right != 0 || bottom != 0)
+				{
+					minimum = min(right, bottom);
+					if (minimum == 0) 
+					{
+						tabColor.at<uchar>(row, col) = max(right, bottom);
+					}
+					else {
+						tabColor.at<uchar>(row, col) = minimum;
+					}
+					if (imax < tabColor.at<uchar>(row, col))
+						imax = tabColor.at<uchar>(row, col);
+				}
+			}
+
+		}
+	}
+	return imax;
 }
+
--- VisionLab2/VisionLab2/VisionLab2.cpp
+++ VisionLab2/VisionLab2/VisionLab2.cpp
@@ -3,6 +3,8 @@
 
 #include <opencv2\opencv.hpp>
 #include "stdafx.h"
+#include "ObjectGrabber.h"
+
 
 using namespace cv;
 using namespace std;
@@ -12,64 +14,71 @@ using namespace std;
 int main()
 {
 
-	Mat hsv_image,mask, mask1, mask2, mask3;
+	ObjectGrabber objGrabber;
+	int nb;
+
+	Mat hsv_image,mask2,mask1, mask;
 
 	
 	Mat picture = imread("trafficlight.jpg", 1);
-	Mat pictureOrig = imread("trafficlight.jpg", 1);
 	vector<Vec4i> hierarchy;
 	vector<vector<Point> > contours;
 	cvtColor(picture, hsv_image, COLOR_BGR2HSV);
 
 
-	//Masque pour avoir les trois couleurs
-	inRange(hsv_image, Scalar(20, 100, 100), Scalar(30, 255, 255), mask1);
-	inRange(hsv_image, Scalar(0, 100, 100), Scalar(10, 255, 255), mask2);
-	inRange(hsv_image, Scalar(45, 50, 50), Scalar(90, 255, 255), mask3);
-	//OU binaire pour ajouter les couleurs
+	////Masque pour avoir les trois couleurs
+	inRange(hsv_image, Scalar(20, 100, 100), Scalar(30, 255, 255), mask2);
+	inRange(hsv_image, Scalar(0, 100, 100), Scalar(10, 255, 255), mask1);
 	bitwise_or(mask1, mask2, mask);
-	bitwise_or(mask, mask3, mask);
-	findContours(mask, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	//trouver les plus gros contours
-	int largestArea = 0;
-	int largestContourIndex = 0;
-	int secondLargestArea = 0;
-	int secondContourIndex = 0;
-
-	//trouver le plus gros
-	for (int i = 0; i < contours.size(); i++)
-	{
-		double area = contourArea(contours[i]);
-		if (area > largestArea) {
-			largestArea = area;
-			largestContourIndex = i;
-		}
-
-	}
-	//trouver le deuxieme plus gros
-	for (int i = 0; i< contours.size(); i++)
-	{
-		double area = contourArea(contours[i]);
-		if (area > secondLargestArea && area < largestArea) {
-			secondLargestArea = area;
-			secondContourIndex = i;
-
-		}
-	}
+	objGrabber.setMat(mask);
+	nb = objGrabber.getNumberObject();
+	imshow("bin", mask);
+	cout << nb;
 	
+	//inRange(hsv_image, Scalar(45, 50, 50), Scalar(90, 255, 255), mask3);
+	////OU binaire pour ajouter les couleurs
+	//bitwise_or(mask1, mask2, mask);
+	//bitwise_or(mask, mask3, mask);
+	//findContours(mask, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	drawContours(picture, contours, largestContourIndex, Scalar(128, 128, 128), -1, 8, hierarchy, 0, Point());
-	drawContours(picture, contours, secondContourIndex , Scalar(112, 128, 144), -1, 8, hierarchy, 0, Point());
+	////trouver les plus gros contours
+	//int largestArea = 0;
+	//int largestContourIndex = 0;
+	//int secondLargestArea = 0;
+	//int secondContourIndex = 0;
 
-	imshow("picture", picture);
-	imshow("Treshold", mask);
-	imshow("originale", pictureOrig);
+	////trouver le plus gros
+	//for (int i = 0; i < contours.size(); i++)
+	//{
+	//	double area = contourArea(contours[i]);
+	//	if (area > largestArea) {
+	//		largestArea = area;
+	//		largestContourIndex = i;
+	//	}
 
+	//}
+	////trouver le deuxieme plus gros
+	//for (int i = 0; i< contours.size(); i++)
+	//{
+	//	double area = contourArea(contours[i]);
+	//	if (area > secondLargestArea && area < largestArea) {
+	//		secondLargestArea = area;
+	//		secondContourIndex = i;
+
+	//	}
+	//}
+	//
+
+	//drawContours(picture, contours, largestContourIndex, Scalar(128, 128, 128), -1, 8, hierarchy, 0, Point());
+	//drawContours(picture, contours, secondContourIndex , Scalar(112, 128, 144), -1, 8, hierarchy, 0, Point());
+
+	//imshow("picture", picture);
+	//imshow("Treshold", mask);
+	//imshow("originale", pictureOrig);
+
+	//waitKey(0);
 	waitKey(0);
-
-
-	return 0;
     return 0;
 }
 
